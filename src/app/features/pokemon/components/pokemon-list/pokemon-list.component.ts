58579721040a6ffbaaa2739aec84a8001e7ce441
Su Tran <vansutran99@gmail.com>
2021-09-24 09:39:55 +0700

    fix(pokemon-list):search pokemon using rxjs

--- src/app/features/pokemon/components/pokemon-list/pokemon-list.component.ts
+++ src/app/features/pokemon/components/pokemon-list/pokemon-list.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core'
-import { PokemonList } from '@features/pokemon/models/pokemon.model'
-import { PokemonService } from '@features/pokemon/services/pokemon.service'
-import { Router } from '@angular/router'
-import { Observable } from 'rxjs'
-import { FormControl } from '@angular/forms'
+import { Component, OnInit } from "@angular/core";
+import { PokemonList } from "@features/pokemon/models/pokemon.model";
+import { PokemonService } from "@features/pokemon/services/pokemon.service";
+import { Router } from "@angular/router";
+import { FormControl } from "@angular/forms";
+import { debounceTime, map, startWith, tap } from "rxjs/operators";
 
 @Component({
   selector: 'app-pokemon-list',
@@ -11,25 +11,38 @@ import { FormControl } from '@angular/forms'
   styleUrls: ['./pokemon-list.component.scss'],
 })
 export class PokemonListComponent implements OnInit {
-  pokemonList!: Observable<PokemonList[]>
-  query = new FormControl('')
+  pokemonList: PokemonList[] = [];
+  filteredPokemon: PokemonList[] = []
+  query = new FormControl('');
 
-  constructor(private _pokeService: PokemonService, private readonly router: Router) {
-    this.pokemonList = this._pokeService.pokemonList$
-  }
+  constructor(private _pokeService: PokemonService, private readonly router: Router) {}
 
   ngOnInit(): void {
-    this.loadPokemonList()
-    this.onSearchPokemon()
+    this.loadPokemonList();
+    this.onSearchPokemon();
   }
 
   loadPokemonList() {
-    this._pokeService.getListPokemon()
+    console.log('load...')
+    this._pokeService.getListPokemon();
+    this._pokeService.pokemonList$.pipe(map(pokemon => {
+      this.pokemonList = pokemon
+      this.filteredPokemon = pokemon
+    })).subscribe()
   }
 
-  onSearchPokemon() {}
+  onSearchPokemon() {
+    this.query.valueChanges.pipe(
+      debounceTime(600),
+      map((searchTerm) => searchTerm.toLowerCase()),
+      tap(search => {
+        this.filteredPokemon = this.pokemonList.filter(pokemon => pokemon.name.english.toLowerCase().includes(search))
+      }),
+      startWith("")
+    ).subscribe()
+  }
 
   onEdit(id: number) {
-    this.router.navigate(['', id, 'edit'])
+    this.router.navigate(['', id, 'edit']);
   }
 }
--- src/app/features/pokemon/services/pokemon.service.ts
+++ src/app/features/pokemon/services/pokemon.service.ts
@@ -1,92 +1,95 @@
-import { Injectable } from '@angular/core'
-import { PokemonList } from '@features/pokemon/models/pokemon.model'
-import DataPokemon from '@features/pokemon/data/pokemon-data'
-import { BehaviorSubject, Observable, of, throwError } from 'rxjs'
-import { HttpClient } from '@angular/common/http'
-import { catchError, map } from 'rxjs/operators'
-import { Router } from '@angular/router'
-import { StorageService } from '@core/services/storage.service'
+import { Injectable } from '@angular/core';
+import { PokemonList } from '@features/pokemon/models/pokemon.model';
+import DataPokemon from '@features/pokemon/data/pokemon-data';
+import { BehaviorSubject, Observable, of, throwError } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { StorageService } from '@core/services/storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PokemonService {
-  private pokemonSubject = new BehaviorSubject<PokemonList[]>([])
-  pokemonList$ = this.pokemonSubject.asObservable()
+  pokemonList$!: Observable<PokemonList[]>;
+  private pokemonSubject = new BehaviorSubject<PokemonList[]>([]);
 
+  favoritePokemon$!: Observable<{ likes: number; dislikes: number }>;
   private favoritePokemonSubject = new BehaviorSubject<any>(
     this.storageService.getValue('status') || { likes: 0, dislikes: 0 },
-  )
-  favoritePokemon$ = this.favoritePokemonSubject.asObservable()
+  );
 
   constructor(
     private router: Router,
     private readonly httpClient: HttpClient,
     private readonly storageService: StorageService,
-  ) {}
+  ) {
+    this.pokemonList$ = this.pokemonSubject.asObservable();
+    this.favoritePokemon$ = this.favoritePokemonSubject.asObservable();
+  }
 
   getListPokemon() {
     const convertPokemon: PokemonList[] = DataPokemon.reduce((acc, cur) => {
-      const infoPoke = { ...cur, type: cur.type.map((t) => t.toLowerCase()) }
+      const infoPoke = { ...cur, type: cur.type.map((t) => t.toLowerCase()) };
       // @ts-ignore
-      acc.push(infoPoke)
-      return acc
-    }, [])
+      acc.push(infoPoke);
+      return acc;
+    }, []);
 
-    return of(
-      this.storageService.getValue<PokemonList[]>('pokemonList') || convertPokemon,
-    ).subscribe((res) => this.pokemonSubject.next(res))
+    return of(this.storageService.getValue<PokemonList[]>('pokemonList') || convertPokemon)
+      .pipe(map((pokemon) => this.pokemonSubject.next(pokemon)))
+      .subscribe();
   }
 
   getPokemon(id: number) {
     return this.pokemonSubject.pipe(
       map((pokemon) => pokemon.find((item) => item.id === Number(id))),
-    )
+    );
   }
 
   getPokemonTypes(): Observable<any> {
     return this.httpClient.get<any>('https://pokeapi.co/api/v2/type').pipe(
       map((res) => {
-        return res.results
+        return res.results;
       }),
       catchError((err) => {
-        return throwError(err)
+        return throwError(err);
       }),
-    )
+    );
   }
 
   nextPokemon(id: number) {
-    this.router.navigate(['', Number(id) + 1])
+    this.router.navigate(['', Number(id) + 1]);
   }
 
   prevPokemon(id: number) {
-    this.router.navigate(['', Number(id) - 1])
+    this.router.navigate(['', Number(id) - 1]);
   }
 
   likePokemon() {
-    const currentValue = this.favoritePokemonSubject.getValue()
+    const currentValue = this.favoritePokemonSubject.getValue();
     const likes = {
       ...currentValue,
       likes: currentValue.likes + 1,
-    }
-    this.storageService.setObject('status', likes)
-    this.favoritePokemonSubject.next(likes)
+    };
+    this.storageService.setObject('status', likes);
+    this.favoritePokemonSubject.next(likes);
   }
 
   dislikePokemon() {
-    const currentValue = this.favoritePokemonSubject.getValue()
+    const currentValue = this.favoritePokemonSubject.getValue();
     const dislikes = {
       ...currentValue,
       dislikes: currentValue.dislikes + 1,
-    }
-    this.storageService.setObject('status', dislikes)
-    this.favoritePokemonSubject.next(dislikes)
+    };
+    this.storageService.setObject('status', dislikes);
+    this.favoritePokemonSubject.next(dislikes);
   }
 
   addPokemon(data: any) {
-    const newPokemon: PokemonList[] = [...this.pokemonSubject.getValue(), data]
-    this.pokemonSubject.next(newPokemon)
-    this.storageService.setObject('pokemonList', newPokemon)
+    const newPokemon: PokemonList[] = [...this.pokemonSubject.getValue(), data];
+    this.pokemonSubject.next(newPokemon);
+    this.storageService.setObject('pokemonList', newPokemon);
   }
 
   // editPokemon(id: number, data: PokemonList) {
